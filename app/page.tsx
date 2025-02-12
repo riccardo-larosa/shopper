20ecdf798341fbb7579d22dbae1750114245846e
Riccardo La Rosa <r.larosa@gmail.com>
2025-02-11 20:03:57 -0500

    initial release

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 import { Thread } from "@/components/assistant-ui/thread";
-import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
-import { AssistantRuntimeProvider } from "@assistant-ui/react";
+// import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { PriceSnapshotTool } from "@/components/tools/price-snapshot/PriceSnapshotTool";
+// import { AssistantRuntimeProvider } from "@assistant-ui/react";
+import { makeMarkdownText } from "@assistant-ui/react-markdown";
+
+const MarkdownText = makeMarkdownText({});
 
 export default function Home() {
-  const runtime = useChatRuntime({ api: "/api/chat" });
+  // const runtime = useChatRuntime({ api: "/api/chat" });
 
   return (
-    <AssistantRuntimeProvider runtime={runtime}>
-      <main className="h-dvh">
-        <Thread />
-      </main>
-    </AssistantRuntimeProvider>
+    <main className="h-full">
+      <Thread 
+        welcome={{
+          suggestions: [
+            {
+              prompt: "How much revenue did Apple make last year?",
+            },
+            {
+              prompt: "Is McDonald's profitable?",
+            },
+            {
+              prompt: "What's the current stock price of Tesla?",
+            },
+          ],
+        }}
+        assistantMessage={{ components: { Text: MarkdownText } }} 
+        tools={[PriceSnapshotTool]}
+      />
+    </main>
   );
 }
